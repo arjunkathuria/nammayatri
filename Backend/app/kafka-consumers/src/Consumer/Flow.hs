d29844c1e711d08f904c3281b66a6170bb87d300
Arjun Kathuria <arjun.kathuria8@gmail.com>
2023-04-06 14:06:28 +0530

    update and fix "streamly" useages and breakages in codebase
    * We updated the package "streamly" to a newer version,
      which caused some breakages due to changes in the
      package imports, interfaces, now deprecated functions
      or types etc.
    
    * We needed to update some of our code to reflect these updates
      and make them compatible with the newer version in order
      for the code to be able to build.
    
    * The version in the stackage snapshot was a bit older,
      so we provisioned newer version from github which had
      some performance fixes for GHC 9.2.x series, which the
      snapshot version was missing.
    
    * Added the type signature for the offending function,
      it was pretty hard to see otherwise.

--- Backend/app/kafka-consumers/src/Consumer/Flow.hs
+++ Backend/app/kafka-consumers/src/Consumer/Flow.hs
@@ -31,7 +31,9 @@ import Kernel.Types.Flow
 import Kernel.Utils.Common (generateGUID, withLogTag)
 import qualified Streamly.Internal.Data.Fold as SF
 import Streamly.Internal.Data.Stream.Serial (SerialT)
-import qualified Streamly.Internal.Prelude as S
+import qualified Streamly.Prelude as S
+
+import Consumer.AvailabilityTime.Types (LocationUpdates)
 
 runConsumer :: L.FlowRuntime -> AppEnv -> ConsumerType -> Consumer.KafkaConsumer -> IO ()
 runConsumer flowRt appEnv consumerType kafkaConsumer = do
@@ -68,11 +70,16 @@ availabilityConsumer flowRt appEnv kafkaConsumer =
         generateGUID
           >>= flip withLogTag (ATProcessor.processData locationUpdate driverId)
 
+    buildTimeSeries :: MonadIO m =>
+      SF.Fold
+      m
+      (LocationUpdates, ConsumerRecordD)
+      ([UTCTime], Maybe ConsumerRecordD)
     buildTimeSeries = SF.mkFold step start extract
       where
-        step (!acc, _) (val, cr) = pure (val.ts : acc, Just cr)
-        start = pure ([], Nothing)
-        extract = pure
+        step (!acc, _) (val, cr) = SF.Partial (val.ts : acc, Just cr)
+        start = SF.Partial ([], Nothing)
+        extract = id
 
 readMessages ::
   (FromJSON a, ConvertUtf8 aKey ByteString) =>
